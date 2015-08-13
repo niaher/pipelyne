b3c6c0b2c859980906c2b0e4fb03542aa5f9ce40
niaher <niaher.crimston@gmail.com>
2015-08-13 16:23:52 +0700

    Feature: TableTransformer to use DataTables plugin.

--- Pipelyne.Core/Transformers/TableTransformer.cs
+++ Pipelyne.Core/Transformers/TableTransformer.cs
@@ -7,29 +7,53 @@
 		public ContentItem Transform(string input)
 		{
 			string html = @"
-<div id='mytable'></div>
+<link href='//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css' rel='stylesheet' />
+<table id='mytable' class='table'>
+	<thead></thead>
+	<tbody></tbody>
+</table>
+<script src='//code.jquery.com/jquery-2.1.4.min.js'></script>
+<script src='//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js'></script>
 <script>
-function tableCreate(el, data)
-{
-    var tbl = document.createElement('table');
-	tbl.className = 'table';
+$(document).ready(function(){
+	function tableCreate(el, data)
+	{
+		var tbl = el;
+		
+		var thead = el.tHead;
+		var tbody = el.getElementsByTagName('tbody')[0];
 
-    for (var i = 0; i < data.length; ++i)
-    {
-        var tr = tbl.insertRow();
+		var firstData = data[0];
+		var tr = thead.insertRow();
 
-		for (var property in data[i]) {
-			if (data[i].hasOwnProperty(property)) {
+		for (var property in firstData) {
+			if (firstData.hasOwnProperty(property)) {
 				var td = tr.insertCell();
-				td.appendChild(document.createTextNode(data[i][property].toString()));
+				td.appendChild(document.createTextNode(property || ''));
 			}
 		}
-    }
-    el.appendChild(tbl);
-}
-var data = " + input + @";
-tableCreate(document.getElementById('mytable'), data);
-</script>";
+
+		for (var i = 0; i < data.length; ++i)
+		{
+			var tr = tbody.insertRow();
+
+			for (var property in data[i]) {
+				if (data[i].hasOwnProperty(property)) {
+					var td = tr.insertCell();
+					td.appendChild(document.createTextNode((data[i][property] || '').toString()));
+				}
+			}
+		}
+	}
+
+	var data = " + input + @";
+	tableCreate(document.getElementById('mytable'), data);
+
+	$('#mytable').DataTable();
+});
+
+</script>
+";
 
 			return new ContentItem(html, "text/html");
 		}
